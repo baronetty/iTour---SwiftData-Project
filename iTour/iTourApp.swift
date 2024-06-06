b42554233ae58b0c07184bd713b7c7066ee59167
Leo Barnett <leo.e.barnett@icloud.com>
2024-06-06 13:20:16 +0200

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Leo  on 06.06.24.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
